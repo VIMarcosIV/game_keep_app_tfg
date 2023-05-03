080c6b1ba880801912d86de7930c556188993c43
VIMarcosIV <msanchezm@iesch.org>
2023-05-03 16:42:14 +0200

    -Splash Screen, Launch Icons, Home Page añadidos

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Home_Page extends StatelessWidget {
+  const Home_Page({Key? key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
--- lib/pages/menu_page.dart
+++ lib/pages/menu_page.dart
@@ -1,5 +1,6 @@
 import 'package:convex_bottom_bar/convex_bottom_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_tfg/pages/home_page.dart';
 
 class Menu_Page extends StatefulWidget {
   const Menu_Page({super.key});
@@ -15,7 +16,7 @@ class _Menu_PageState extends State<Menu_Page> {
   Widget build(BuildContext context) {
     var _pageOption = [
       // Aqui van las screens
-      // Home_Page(),
+      Home_Page(),
       // UpcomingMovies_Page(),
       // PopularMovies_Page(),
       // BestMovies_Page(),
@@ -23,21 +24,21 @@ class _Menu_PageState extends State<Menu_Page> {
     ];
     return Scaffold(
       appBar: AppBar(
-          title: Text("TMDB APP"),
+          title: Text("TFG APP"),
           titleTextStyle: TextStyle(
             color: Colors.white,
             fontSize: 20,
             fontWeight: FontWeight.bold,
           ),
-          backgroundColor: Colors.orange[400]),
+          backgroundColor: Colors.redAccent),
       body: _pageOption[selectedPage],
       bottomNavigationBar: ConvexAppBar(
-        backgroundColor: Colors.orange[400],
+        backgroundColor: Colors.redAccent,
         color: Colors.white,
         height: 47,
         items: [
           // Aqui van los items que saldran abajo en la NavBar
-          // TabItem(icon: Icons.home, title: "Home"),
+          TabItem(icon: Icons.home, title: "Home"),
           // TabItem(icon: Icons.new_releases, title: "News"),
           // TabItem(icon: Icons.people_alt, title: "Popular"),
           // TabItem(icon: Icons.assessment, title: "T.Rated"),
