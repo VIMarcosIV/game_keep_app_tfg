dbd5fd4f8c3f6df441868904a9c351766782a8d8
VIMarcosIV <msanchezm@iesch.org>
2023-05-16 00:21:11 +0200

    Barra de busqueda funcional, pero estilo bugeado

--- lib/pages/gird_page.dart
+++ lib/pages/gird_page.dart
@@ -1,29 +1,83 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-class Grid_Page extends StatelessWidget {
-  const Grid_Page({Key? key});
+class Grid_Page extends StatefulWidget {
+  const Grid_Page({Key? key}) : super(key: key);
+
+  @override
+  _Grid_PageState createState() => _Grid_PageState();
+}
+
+class _Grid_PageState extends State<Grid_Page> {
+  late final TextEditingController searchController;
+  String searchText = '';
+
+  @override
+  void initState() {
+    super.initState();
+    searchController = TextEditingController();
+    searchController.addListener(() {
+      setState(() {
+        searchText = searchController.text;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    searchController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Grid Screen'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () {
+              // Realizar la búsqueda aquí
+            },
+          ),
+        ],
+        bottom: PreferredSize(
+          preferredSize: Size.fromHeight(56.0),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            child: TextField(
+              controller: searchController,
+              decoration: InputDecoration(
+                hintText: 'Buscar por título',
+              ),
+            ),
+          ),
+        ),
       ),
       body: StreamBuilder<QuerySnapshot>(
         stream:
             FirebaseFirestore.instance.collection('videojuegos').snapshots(),
         builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
           if (snapshot.hasError) {
-            return Text('Error al cargar los datos');
+            return SnackBar(
+              content: Text('Error al cargar los datos'),
+              backgroundColor: Colors.red,
+            );
           }
 
           if (snapshot.connectionState == ConnectionState.waiting) {
             return CircularProgressIndicator();
           }
 
+          // Filtrar los resultados según el texto de búsqueda
+          final filteredDocs = snapshot.data!.docs.where((doc) {
+            final title = doc['title'] as String;
+            return title.toLowerCase().startsWith(searchText.toLowerCase());
+          }).toList();
+
           return GridView.builder(
-            itemCount: snapshot.data!.docs.length,
+            itemCount: filteredDocs.length,
             gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
               crossAxisCount: 2,
               mainAxisSpacing: 12.0,
@@ -32,10 +86,9 @@ class Grid_Page extends StatelessWidget {
             ),
             padding: EdgeInsets.all(16.0),
             itemBuilder: (BuildContext context, int index) {
-              DocumentSnapshot document = snapshot.data!.docs[index];
-              //Añadir el resto de datos
-              String title = document['title'];
-              String poster = document['poster'];
+              DocumentSnapshot document = filteredDocs[index];
+              String title = document['title'] as String;
+              String poster = document['poster'] as String;
               return gridItemWidget(context, title, poster);
             },
           );
@@ -56,9 +109,16 @@ Widget gridItemWidget(BuildContext context, String title, String poster) {
     child: Column(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        Image.network(
-          poster,
-          scale: 6,
+        Container(
+          width: 110.0, // Tamaño fijo del contenedor de la imagen
+          height: 110.0, // Tamaño fijo del contenedor de la imagen
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(10.0), // Radio de borde deseado
+            child: Image.network(
+              poster,
+              fit: BoxFit.contain, // Ajuste de la imagen dentro del contenedor
+            ),
+          ),
         ),
         Padding(
           padding: EdgeInsets.symmetric(horizontal: 8.0, vertical: 10.0),
@@ -66,7 +126,7 @@ Widget gridItemWidget(BuildContext context, String title, String poster) {
             title,
             textAlign: TextAlign.center,
             style: theme.textTheme.headline1!.copyWith(
-              fontSize: 14.0,
+              fontSize: 10.0,
               color: Colors.white,
             ),
           ),
@@ -74,4 +134,10 @@ Widget gridItemWidget(BuildContext context, String title, String poster) {
       ],
     ),
   );
+}
+
+void main() {
+  runApp(MaterialApp(
+    home: Grid_Page(),
+  ));
 }
